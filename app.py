5301e4908ee4212299ef6d4d90119ddf0d195a8f
Chirine <chirine.nouar@outlook.fr>
2026-01-28 14:20:14 +0100

    test app

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import streamlit as st
 
-# config de la page
+# ⚠️ Toujours mettre la config en tout début de fichier
 st.set_page_config(
-    page_title="Loan aproval Prediction",
+    page_title="Loan Approval Prediction",
     page_icon="🚀",
     layout="wide"
 )
 
-# titre 
-st.title("Loan Approval Prediction App 🚀")
-
 # contenu
-st.write("Bienvenue dans l'application de prédiction d'approbation de prêt. ")
+st.write("Bienvenue dans l'application de prédiction d'approbation de prêt.")
 
-st.title("About")
+# mot de passe
+password = st.text_input("Entrez le mot de passe pour accéder à l'application :", type="password")
+
+if password == st.secrets["password"]:
+    st.success("Accès accordé ! Vous pouvez maintenant utiliser l'application.")
+
+    # titre
+    st.title("Loan Approval Prediction App 🚀")
+
+else:
+    st.error("Mot de passe incorrect. Veuillez réessayer.")
+    st.stop()
